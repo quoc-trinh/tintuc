70b26741b5d1690e56501adedad36ed1298dd3d1
quoctrinh <quoc.trinh@outlook.com>
2019-01-28 18:48:20 +0100

    Updates

--- precache-manifest.b7c383a699d3bec385c0df975fcaee0c.js
+++ precache-manifest.b7c383a699d3bec385c0df975fcaee0c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "0f81351f9181c584a026",
-    "url": "/tintuc/static/css/main.6c6aaaf4.chunk.css"
+    "revision": "560591ece648901be54c",
+    "url": "/tintuc/static/css/main.8e79e961.chunk.css"
   },
   {
-    "revision": "0f81351f9181c584a026",
-    "url": "/tintuc/static/js/main.0f81351f.chunk.js"
+    "revision": "560591ece648901be54c",
+    "url": "/tintuc/static/js/main.560591ec.chunk.js"
   },
   {
     "revision": "1a57665225f7640b7e70",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/tintuc/static/js/runtime~main.b3b54679.js"
   },
   {
-    "revision": "d4c13843dc16a7c94c1394b0999462df",
+    "revision": "5463d13ebad7c5e8bb59d9020839f3c8",
     "url": "/tintuc/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tintuc/precache-manifest.bc3a65bd19686407c6a4fb9a3ebef8c8.js"
+  "/tintuc/precache-manifest.b7c383a699d3bec385c0df975fcaee0c.js"
 );
 
 workbox.clientsClaim();
